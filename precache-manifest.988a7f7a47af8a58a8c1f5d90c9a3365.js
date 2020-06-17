8649f577d43baa987a7763e176492ef7967cbd8b
chenhe-henry <chenhe.elec@gmail.com>
2020-06-17 23:53:57 +1000

    Updates

---
+++ precache-manifest.988a7f7a47af8a58a8c1f5d90c9a3365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66441cbd553eda466a13e19b71410b4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fbd17f06765545f869be",
+    "url": "/monsters-rolodex/static/css/main.bf63b76e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbd17f06765545f869be",
+    "url": "/monsters-rolodex/static/js/main.1c5432e6.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
